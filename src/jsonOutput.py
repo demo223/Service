163a95ddc703983a30866784498c30841697f4e8
周岐 <51960981+demo223@users.noreply.github.com>
2021-04-03 13:55:52 +0800

    Update jsonOutput.py

--- src/jsonOutput.py
+++ src/jsonOutput.py
@@ -7,16 +7,22 @@ with open('./data/Test2.json', 'r') as f:
 json_str = json.dumps(data1, ensure_ascii=False)  # json对象
 # 存储数据
 output_path = "data/jsonTxt2.txt"
+head_word = []  # 存放中心词
 with open(output_path, 'w', encoding='utf-8') as o:
     for index in range(0, 275):
         qas = data1[index]["qas"][0]
         for key in qas:
             if len(key["answers"]) == 1:
                 print(key["question"] + " : " + key["answers"][0]["text"])
-                o.write(key["question"] + " : " + key["answers"][0]["text"]+"\n")
+                o.write(key["question"] + " : " + key["answers"][0]["text"] + "\n")
+                if key["question"] == "中心词":
+                    head_word.append(key["answers"][0]["text"])
             else:
                 for num in range(len(key["answers"])):
                     print(key["question"] + " : " + key["answers"][num]["text"])
-                    o.write(key["question"] + " : " + key["answers"][num]["text"]+"\n")
+                    o.write(key["question"] + " : " + key["answers"][num]["text"] + "\n")
+                    if key["question"] == "中心词":
+                        head_word.append(key["answers"][num]["text"])
         print("---------------------------------")
-        o.write("---------------------------------"+"\n")
+        o.write("---------------------------------" + "\n")
+print(set(head_word))   # 输出所有不重复的中心词
