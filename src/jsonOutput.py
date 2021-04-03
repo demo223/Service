ea117d46ef954e86c527327ac19a12389a179837
周岐 <51960981+demo223@users.noreply.github.com>
2021-04-03 14:03:31 +0800

    Update jsonOutput.py

--- src/jsonOutput.py
+++ src/jsonOutput.py
@@ -9,7 +9,7 @@ json_str = json.dumps(data1, ensure_ascii=False)  # json对象
 output_path = "data/jsonTxt2.txt"
 head_word = []  # 存放中心词
 with open(output_path, 'w', encoding='utf-8') as o:
-    for index in range(0, 275):
+    for index in range(0, 276):
         qas = data1[index]["qas"][0]
         for key in qas:
             if len(key["answers"]) == 1:
